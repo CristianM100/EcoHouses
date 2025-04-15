3c444c236e2ed2ae65e4c74db4a3609f2f7c0150
CristianM100 <cristian.marinescu92@yahoo.com>
2025-04-15 12:01:08 +0200

    remove com

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,12 @@
 import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFacebook, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { LiaAddressBookSolid } from "react-icons/lia";
 
 const Footer = () => {
   return (
     <footer className="bg-white text-black py-6">
-      <div className="container mx-auto flex justify-between gap-6">
+      <div className="container mx-auto flex text-center justify-between gap-6">
         
         <div className="w-1/3">
           <h5 className="text-lg">EcoHouses</h5>
@@ -15,7 +16,7 @@ const Footer = () => {
           </div>
         </div>
 
-        <div className="w-1/3">
+        <div className="w-3/3">
           <h3 className="text-xl font-semibold">Quick Links</h3>
           <ul>
             <li><Link to="/home" className="text-stone-400 hover:text-white">Home</Link></li>
@@ -24,11 +25,15 @@ const Footer = () => {
           </ul>
         </div>
 
-        <div className="w-1/3">
-          <h3 className="text-xl font-semibold">Follow Us</h3>
-          <ul className="flex gap-4">
-            <li><FontAwesomeIcon icon={faFacebook} size="lg" className="text-stone-400 hover:text-white" /></li>
-            <li><FontAwesomeIcon icon={faLinkedin} size="lg" className="text-stone-400 hover:text-white" /></li>
+        <div className="w-1/3 flex flex-col items-start">
+          <h3 className="text-xl font-semibold mb-2 text-left">Follow Us</h3>
+          <ul className="flex gap-4 items-center">
+            <li>
+              <FontAwesomeIcon icon={faFacebook} size="lg" className="text-stone-400 hover:text-white" />
+            </li>
+            <li>
+              <FontAwesomeIcon icon={faLinkedin} size="lg" className="text-stone-400 hover:text-white" />
+            </li>
           </ul>
         </div>
       </div>
