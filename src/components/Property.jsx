caf030c72c58bb2dd2e4109db118d6a99663e26b
CristianM100 <cristian.marinescu92@yahoo.com>
2025-04-15 12:01:36 +0200

    create Property

--- src/components/Property.jsx
+++ src/components/Property.jsx
@@ -1,13 +1,53 @@
+import React from "react";
+import { BsStarFill } from "react-icons/bs";
+import {Link} from "react-router-dom";
 
+export function Property({ title, images, location, likes, description, facilities, sustainability, price, type })  {
+    return (
+        <Link to='' className="border border-gray-400 rounded-lg p-3">
 
-
-
-export function Property() {
-    return(
-        <>
-
-        </>
-    )
+            <div className="relative">
+                <div className="grad absolute w-full h-full rounded-b-[1.3rem]"></div>
+                    <div className="flex ">
+                        {/* Background */}
+                        <img
+                            src={images}
+                            alt=""
+                            className="object-cover rounded-[1.3rem] sm:h-[17rem]  md:h-[13rem] w-full"
+                        />
+                        {/* Title */}
+                        <div className="absolute text-white font-bold bottom-6 left-6 text-[22px] flex items-center gap-2">
+                            {title}
+                            <span>&#x2022;</span>
+                            <p className="text-[18px] text-slate-200"> ${price}</p>
+                        </div>
+                    </div>
+            </div>
+            {/* Description */}
+            <div className="pt-3 flex justify-between items-start">
+                {/* Left */}
+                <div className="">
+                    <p className="max-w-[17rem] font-semibold text-[17px]">
+                        This is a rare find
+                    </p>
+                    <p className="max-w-[17rem]  text-[16px] -mt-1 text-gray-500">
+                        Jan 28 - Aug 9
+                    </p>
+                    <p className="max-w-[17rem] font-semibold text-[17px]">${price}</p>
+                </div>
+                {/* Right */}
+                <div className="flex items-center space-x-1">
+                    <div>
+                        <div className="flex items-center space-x-1">
+                            <BsStarFill />
+                            <p className="text-[15px]">5.0</p>
+                        </div>
+                        <p className="text-[15px] rounded p-1 bg-orange-300">{type}</p>
+                    </div>
+                </div>
+            </div>
+        </Link>
+      );    
 }
 
 export default Property;
