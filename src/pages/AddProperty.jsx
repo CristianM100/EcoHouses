abbb6679346c7b22a4bc1fa41580875209f7c559
CristianM100 <cristian.marinescu92@yahoo.com>
2025-04-17 21:08:55 +0200

    create AddProperty

---
+++ src/pages/AddProperty.jsx
@@ -0,0 +1,35 @@
+import AddPropertyForm from '../components/AddPropertyForm';
+import { Link } from "react-router-dom";
+import Footer from "../components/Footer";
+
+
+export function AddProperty() {
+    return(
+        <>
+            <header className='flex text-center  z-10 py-10 px-5 text-stone-400'>
+            <nav className='container flex justify-between ml-10'>
+                <div>
+                    <Link 
+                        to='/'
+                        className='text-green-800 text-2xl font-bold tracking-widest'
+                    >
+                        EcoHomes
+                    </Link>
+                </div>
+
+                <ul className='text-black flex items-center gap-30'>
+                    <li className='ml-2 text-sm font-medium uppercase tracking-wider'>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    
+                </ul>    
+            </nav>
+        </header>
+    
+            <AddPropertyForm />
+            <Footer />
+        </>
+    )
+}
+
+export default AddProperty;
