504ba309d7a67a4c48379583f4ed3c6f934f4bc8
CristianM100 <cristian.marinescu92@yahoo.com>
2025-04-17 21:09:08 +0200

    edit home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
-
-
-
+import PropertyList from '../components/PropertyList';
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 export function Home() {
     return(
         <>
-            
+            <Header />
+            <div>this is home page</div>
+            <PropertyList filterProperties="all"/>
+            <Footer />
         </>
     )
 }
