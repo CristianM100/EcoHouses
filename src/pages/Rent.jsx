8a9655103886fb9e9ccf01eb6e01f60987d11aa3
CristianM100 <cristian.marinescu92@yahoo.com>
2025-04-17 21:09:31 +0200

    edit rent page

--- src/pages/Rent.jsx
+++ src/pages/Rent.jsx
@@ -1,11 +1,16 @@
-
+import PropertyList from "../components/PropertyList";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 
 
 export function Rent() {
     return(
         <>
-            
+            <Header />
+            <div>this is rent page</div>
+            <PropertyList filterProperties="for rent"/> 
+            <Footer />   
         </>
     )
 }
