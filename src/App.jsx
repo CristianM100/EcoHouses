d4dc87f63dffe87434a150f2dde7c4314ef90c44
CristianM100 <cristian.marinescu92@yahoo.com>
2025-04-17 21:06:05 +0200

    modify App

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,24 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import PropertyList from './components/PropertyList';
-//import './App.css';
+import Rent from './pages/Rent';
+import Sale from './pages/Sale';
+import Home from './pages/Home';
+import Individual from './pages/Individual';
+import { PropertyProvider } from './context/PropertyContext';
+import AddProperty from './pages/AddProperty';
 
 function App() {
   return (
     <div className=''>
       <Router> 
-        <Header />
-        {/*<Routes>
-          <Route path="/" element={<Hero />} />
-        
-        </Routes>*/}
-        <PropertyList />
-        <Footer />
+        <PropertyProvider>
+          <Routes>
+            <Route path="/" element={<Home  />} />
+            <Route path="/rent" element={<Rent  />} />
+            <Route path="/sale" element={<Sale  />} />
+            <Route path="/properties/:id" element={<Individual  />} />
+            <Route path="/add-property" element={<AddProperty />} />
+          </Routes>
+        </PropertyProvider>
      </Router>
     </div>
   );
